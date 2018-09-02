f1eb0209aa085339d4fc43b74fed4a2739d26530
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-01 23:03:22 -0500

    Add links, teaching base

---
+++ teaching/16Fall/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Teaching - Ben Reiniger - Fall 2016</title>
+</head>
+<body lang="EN-US">
+
+<div id="navmaincontainer">
+<!-- THE MENU -->
+<nav id="navbar"></nav>
+<script type="text/javascript" src="./includes/header+menu.js"></script>
+
+<!-- THE BODY COLUMN -->
+<div id="main">
+<h2>Math 251<br>Multivariate Calculus</h2>
+<h3>Fall 2016</h3>
+<table border="1">
+<tr><td><b>Section:</b>  </td><td><b>01</b></td>                      <td><b>03</b></td></tr>
+<tr><td><b>Meetings:</b> </td><td>MWF 11:25-12:40</td> <td>MW 3:15-4:30, F 1:50-3:05</td></tr>
+<tr><td><b>Classroom:</b></td><td>3A2-1 Tech South</td> <td>005 Hermann Hall</td></tr>
+<tr><td><b>Syllabus:</b> </td><td><a href="syllabus01.pdf">link</a></td><td><a href="syllabus03.pdf">link</a></td></tr>
+</table>
+
+<p>
+<b>E-mail:</b> breiniger <i>(at)</i> iit <i>(dot)</i> edu <br>
+<b>My office:</b> 110 Rettaliata Engineering Building<br>
+<b>Office hours:</b> M2-3, T11:25-12:40, and by appointment </p>
+
+<p style="margin-bottom:0pt">
+Advice for WebAssign:</p>
+<ol>
+<li>You should first attempt each assigned problem on paper (log off the WebAssign website)
+like regular paper and pen assignments. Save this written work for review before quizzes and
+exams and in case you have to discuss your solution with me.
+<li>Carefully read the WebAssign Student Guide to understand how to format your solutions.
+<li>When you have finished entering your answers, click on "Submit" and WebAssign will immediately 
+grade your solutions. You can then re-work your incorrect problems and re-submit
+them to improve your score. You can submit your assignment up to 5 times without penalty.
+<li>After two incorrect attempts, look at the book again, review your notes and re-do the problem
+from scratch. Do not guess an answer. Remember you have only 5 attempts for each problem,
+so do not waste your attempts on frivolous trial and error solutions.
+<li>Do not enter an equivalent expression when your answer is marked wrong; the software behind
+ Webassign matches equivalent expressions.
+<li>Avoid simple mistakes like: wrong syntax, incorrect usage of parentheses, incorrect letter 
+for a parameter or variable.
+<li>If you have technical difficulties with the WebAssign website, please contact their technical
+support directly online.
+</ol>
+
+
+<p><a href="quizzes">Quizzes &amp; solutions</a></p>
+
+<div>
+ <a href="ex1practice.pdf">Practice exam 1</a> and <a href="ex1practice-Sols.pdf">Solutions</a>.
+ <a href="Exam1.pdf">Exam 1</a> and <a href="Exam1Sols.pdf">Solutions</a>.<br>
+ Errata to the practice exam: 
+   <ul><li>Solutions #3: Near the end, the projection should have $|n|$ in the denominator instead of $|v|$, which (together with fixing an arithmetic mistake) leads to a final answer of $\sqrt{5}$.
+       <li>#3: $\ell_1$ is not the same as the one above.  Use this one.
+       <li>#13d: The numerator was meant to be $x^3 y$.  (As written, the limit would be zero.)
+       <li>#14: I meant to also ask about $f_y$ at the upper-central point.
+       <li>#16: It makes more sense to approximate $f(1.1,0.1)$.  Anyway, the problem isn't wrong as written, you're just less likely to get a "good" approximation.
+   </ul>
+</div>
+<div style="margin-top: 1em;">
+ <a href="ex2practice.pdf">Practice exam 2</a> and <a href="ex2practice-Sols.pdf">Solutions</a>.
+ <a href="Exam2.pdf">Exam 2</a> and <a href="Exam2Sols.pdf">Solutions</a>.<br>
+ Errata to the practice exam solutions: In #14, the lower bound on $y$ should be just $r^2$.
+</div>
+<div style="margin-top: 1em;">
+ <a href="ch16practice.pdf">Practice for chapter 16</a> (answers collaboratively produced in Piazza; ignore 2b).
+</div>
+<div style="margin-top: 1em;">
+ <a href="FinalExam.pdf">Final Exam</a>.
+</div>
+
+
+<!-- Last updated section -->
+<p class="lastupdate">Last modified November 3, 2017.</p>
+</div><!--end main-->
+</div><!--end navmaincontainer-->
+
+
+<!-- THE RIGHT COLUMN -->
+<div id="right">
+</div>
+
+
+<!-- W3 validator -->
+<div id="footer">
+  <p>
+    [<a href="http://validator.w3.org/check?uri=referer">HTML5</a>]
+  </p>
+</div>
+
+</body>
+</html>
