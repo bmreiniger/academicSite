058114542618feff9ab0d3f2401df92b0b0466a0
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-01 22:14:17 -0500

    (testing includes)

--- includes/menu.js
+++ includes/menu.js
@@ -1,13 +1,10 @@
-var menu = 
-"<nav id="navbar">
-  <ul>
-  <li><a href="/academic">Home</a></li>
-  <li><a href="/academic/aboutme.php">About Me</a></li>
-  <li><a href="/">Coding <span class="fas fa-external-link-alt"></span></a></li>
-  <li><a href="/academic/research">Research</a></li>
-  <li><a href="/academic/teaching">Teaching</a>
-  <li><a href="/academic/links.php">Links</a></li>
-  </ul>
-</nav>";
+var menu = "  <ul>"
+menu += "  <li><a href='/academic'>Home</a></li>"
+menu += "  <li><a href='/academic/aboutme.php'>About Me</a></li>"
+menu += "  <li><a href='/'>Coding <span class='fas fa-external-link-alt'></span></a></li>"
+menu += "  <li><a href='/academic/research'>Research</a></li>"
+menu += "  <li><a href='/academic/teaching'>Teaching</a>"
+menu += "  <li><a href='/academic/links.php'>Links</a></li>"
+menu += "  </ul>"
 
-document.getElementByID("menu").innerHTML = menu
+document.getElementByID("navbar").innerHTML = menu;
