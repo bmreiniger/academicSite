4d269ecb7b41de79820d5b0f8d6b3636603a3699
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-01 23:48:20 -0500

    fix relative links

--- includes/header+menu.js
+++ includes/header+menu.js
@@ -1,10 +1,10 @@
 var menu = "  <ul>"
 menu += "  <li><a href='/academicSite'>Home</a></li>"
-menu += "  <li><a href='/academicSite/aboutme.php'>About Me</a></li>"
+menu += "  <li><a href='/academicSite/aboutme.html'>About Me</a></li>"
 menu += "  <li><a href='/'>Coding <span class='fas fa-external-link-alt'></span></a></li>"
-menu += "  <li><a href='/academicSite/research'>Research</a></li>"
+menu += "  <li><a href='/academicSite/research/index.html'>Research</a></li>"
 menu += "  <li><a href='/academicSite/teaching'>Teaching</a>"
-menu += "  <li><a href='/academicSite/links.php'>Links</a></li>"
+menu += "  <li><a href='/academicSite/links.html'>Links</a></li>"
 menu += "  </ul>"
 
 var headerstuff = `
