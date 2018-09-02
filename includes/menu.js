59f07f60b0c4bc95719848604279976f72577620
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-01 21:40:44 -0500

    Initial commit

---
+++ includes/menu.js
@@ -0,0 +1,13 @@
+var menu = 
+"<nav id="navbar">
+  <ul>
+  <li><a href="/academic">Home</a></li>
+  <li><a href="/academic/aboutme.php">About Me</a></li>
+  <li><a href="/">Coding <span class="fas fa-external-link-alt"></span></a></li>
+  <li><a href="/academic/research">Research</a></li>
+  <li><a href="/academic/teaching">Teaching</a>
+  <li><a href="/academic/links.php">Links</a></li>
+  </ul>
+</nav>";
+
+document.getElementByID("menu").innerHTML = menu
