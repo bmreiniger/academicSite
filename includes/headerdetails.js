c4e28021685b832f484ddf87466892a36ab68b9b
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-01 22:49:18 -0500

    (testing includes)

--- includes/headerdetails.js
+++ includes/headerdetails.js
@@ -1,17 +1,7 @@
-var menu = "  <ul>"
-menu += "  <li><a href='/academic'>Home</a></li>"
-menu += "  <li><a href='/academic/aboutme.php'>About Me</a></li>"
-menu += "  <li><a href='/'>Coding <span class='fas fa-external-link-alt'></span></a></li>"
-menu += "  <li><a href='/academic/research'>Research</a></li>"
-menu += "  <li><a href='/academic/teaching'>Teaching</a>"
-menu += "  <li><a href='/academic/links.php'>Links</a></li>"
-menu += "  </ul>"
-
-var headerstuff = `
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <meta name="viewport" content="width=device-width" />
 <link rel="stylesheet" href="/academic/css/style.css" />
-<link rel="icon" type="image/ico" href="/academic/images/favicon.ico" />
+<link rel="icon" type="image/ico" href="academic/images/favicon.ico" />
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <script type="text/x-mathjax-config">
   MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['//(','//)']]}});
@@ -28,8 +18,4 @@ var headerstuff = `
     ga('create', 'UA-90850830-1', 'auto');
     ga('send', 'pageview');
   </script>
-`;
 
-headerstuff += menu;
-
-document.getElementById("navbar").innerHTML = headerstuff;
