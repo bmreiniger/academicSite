4bf4794639ecec9eb940decb8e68b2e795138929
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-05 21:59:32 -0500

    Lessen js for inclusions, fix links

--- includes/header+menu.js
+++ includes/header+menu.js
@@ -7,28 +7,4 @@ menu += "  <li><a href='/academicSite/teaching'>Teaching</a>"
 menu += "  <li><a href='/academicSite/links.html'>Links</a></li>"
 menu += "  </ul>"
 
-var headerstuff = `
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<meta name="viewport" content="width=device-width" />
-<link rel="stylesheet" href="/academicSite/css/style.css" />
-<link rel="icon" type="image/ico" href="/academicSite/images/favicon.ico" />
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-<script type="text/x-mathjax-config">
-  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['//(','//)']]}});
-</script>
-<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-</script>
-<!-- Google Analytics -->
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125133600-2"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-
-	  gtag('config', 'UA-125133600-2');
-</script>
-`;
-
-document.getElementsByTagName('head')[0].appendChild(headerstuff);
 document.getElementById("navbar").innerHTML = menu;
