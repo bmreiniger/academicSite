65c53f888e5ac2e996ac7e3fbb670c1af6b51426
Ben Reiniger <bmreiniger@users.noreply.github.com>
2018-09-07 21:07:58 -0500

    Fix js

--- includes/MathJax.js
+++ includes/MathJax.js
@@ -1,5 +1,18 @@
-<script type="text/x-mathjax-config">
-  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['//(','//)']]}});
-</script>
-<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-</script>
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js";
+
+  var config = 'MathJax.Hub.Config({' +
+                 'extensions: ["tex2jax.js"],' +
+                 'jax: ["input/TeX","output/HTML-CSS"]' +
+               '});' +
+               'MathJax.Hub.Startup.onload();';
+
+  if (window.opera) {script.innerHTML = config}
+               else {script.text = config}
+
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
+//MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['//(','//)']]}});
